8df6ed165c081a6110605b8186db5e9f092b12d9
Menschikov Eduard <psix0zz90@gmail.com>
2016-11-19 16:27:46 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-	var link = document.querySelector(".holels-search");
+	var link = document.querySelector("h2");
 	var popup = document.querySelector(".search-window");
 	popup.classList.add("search-window-hidden");
 
 	link.addEventListener("click", function(event) {
 	event.preventDefault();
 	console.log("Click!")
-	popup.classList.toggle("search-window-show");
-	      });
+	popup.classList.toggle("search-window-hidden");
+	      })
